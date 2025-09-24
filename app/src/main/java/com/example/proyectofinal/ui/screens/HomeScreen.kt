4c29d988a363f9e4df2d179ebcbdb5de4217567b
Alexander <villafuertequispealex@gmail.com>
2025-09-24 00:54:43 -0500

    Pantalla home

---
+++ app/src/main/java/com/example/proyectofinal/ui/screens/HomeScreen.kt
@@ -0,0 +1,60 @@
+package com.example.proyectofinal.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun HomeScreen() {
+    Scaffold() { innerPadding ->
+        var texto by remember { mutableStateOf("") }
+
+        Column (
+            // padding
+            modifier = Modifier.fillMaxSize().padding(innerPadding),
+            // Elementos centrados
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Bienvenido",
+                fontSize = 24.sp, // tamaño de texto
+                fontWeight = FontWeight.Bold // negrita
+            )
+            Spacer(modifier = Modifier.height(24.dp)) // espaciado
+            TextField(
+                value = texto,
+                onValueChange = { texto = it },
+                label = { Text("Escribe algo") } // placeholder
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            Button(onClick = {}) {
+                Text(text = "Enviar Texto")
+            }
+        }
+    }
+}
+@Preview(showSystemUi = true)
+@Composable
+fun Preview() {
+    HomeScreen()
+}
