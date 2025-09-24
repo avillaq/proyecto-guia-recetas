86558cd6dfc68711b30e2264b633d2b7b1bf20c2
Alexander <villafuertequispealex@gmail.com>
2025-09-24 13:37:22 -0500

    Cambio en el nombre de los objetos

--- app/src/main/java/com/example/proyectofinal/navigation/AppScreens.kt
+++ app/src/main/java/com/example/proyectofinal/navigation/AppScreens.kt
@@ -1,6 +1,6 @@
 package com.example.proyectofinal.navigation
 
 sealed class AppScreens(val route: String) {
-    object HomeScreen : AppScreens("home")
-    object ResultScreen : AppScreens("result/{mensaje}")
+    object Home : AppScreens("home")
+    object Result : AppScreens("result/{mensaje}")
 }
