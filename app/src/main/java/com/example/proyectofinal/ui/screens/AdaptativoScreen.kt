46cbb713101173b7e3aeec345ccfaea791152e92
Alexander <villafuertequispealex@gmail.com>
2025-09-30 16:43:03 -0500

    Tamaño de fuente adaptativo

--- app/src/main/java/com/example/proyectofinal/ui/screens/AdaptativoScreen.kt
+++ app/src/main/java/com/example/proyectofinal/ui/screens/AdaptativoScreen.kt
@@ -7,38 +7,35 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 @Composable
 fun AdaptativoScreen() {
-    Scaffold() { innerPadding ->
-        Column(
-            // padding
-            modifier = Modifier.fillMaxSize().padding(innerPadding),
-            // Elementos centrados
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = "Bienvenido",
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold
-            )
-            Spacer(modifier = Modifier.height(24.dp))
-            Button(onClick = {}) {
-                Text(text = "Mi boton")
+        Scaffold() { innerPadding ->
+            Column(
+                // padding
+                modifier = Modifier.fillMaxSize().padding(innerPadding),
+                // Elementos centrados
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Bienvenido",
+                    style = MaterialTheme.typography.headlineMedium, // Respeta tamaño de fuente del sistema
+                )
+                Spacer(modifier = Modifier.height(24.dp))
+                Button(onClick = {}) {
+                    Text(text = "Mi boton")
+                }
             }
         }
-    }
 }
 
 @Preview
