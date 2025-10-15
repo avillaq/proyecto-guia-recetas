1d2cf752b56ed5834bf310a9689d167a8cfc4b4f
Alexander <villafuertequispealex@gmail.com>
2025-10-15 11:55:17 -0500

    Codigo base CirculoScreen

---
+++ app/src/main/java/com/example/proyectofinal/ui/screens/CirculoScreen.kt
@@ -0,0 +1,24 @@
+package com.example.proyectofinal.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CirculoScreen() {
+    Scaffold() { innerPadding ->
+        Column (
+            modifier = Modifier.fillMaxSize().padding(innerPadding),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // circulo y botones
+        }
+    }
+}
+
