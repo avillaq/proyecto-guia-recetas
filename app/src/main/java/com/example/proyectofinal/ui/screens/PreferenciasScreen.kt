0f883452d61a3bfb4f60680f3b8f9cf5b8273d24
Alexander <villafuertequispealex@gmail.com>
2025-10-28 16:48:05 -0500

    Creacion pantalla de las preferencias

---
+++ app/src/main/java/com/example/proyectofinal/ui/screens/PreferenciasScreen.kt
@@ -0,0 +1,69 @@
+package com.example.proyectofinal.ui.screens
+
+import android.widget.Toolbar
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PreferenciasScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar (
+                title = { Text("Preferencias de Usuario") },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                )
+            )
+        }
+    ) { innerPadding ->
+        Column (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(innerPadding)
+                .padding(horizontal = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text("Tema", fontSize = 20.sp)
+
+                Switch(
+                    checked = true,
+                    onCheckedChange = {  }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun PreferenciasScreenPreview() {
+    PreferenciasScreen()
+}
