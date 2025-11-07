8fa03ad5a1cb3ee48844ab829beca133f425e9dd
Alexander <villafuertequispealex@gmail.com>
2025-11-07 17:27:24 -0500

    Pantallas CRUD de recetas y acciones de admin

---
+++ app/src/main/java/com/example/proyectofinal/ui/screens/crud/ListaRecetasScreen.kt
@@ -0,0 +1,41 @@
+package com.example.proyectofinal.ui.screens.crud
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ListaRecetasScreen (onNavigate: () -> Unit) {
+    Scaffold(
+        topBar = {
+            TopAppBar (
+                title = { Text("Lista de recetas") },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                )
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.fillMaxSize().padding(innerPadding).padding(horizontal = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            /*
+            Lista de recetas
+            */
+        }
+    }
+}
