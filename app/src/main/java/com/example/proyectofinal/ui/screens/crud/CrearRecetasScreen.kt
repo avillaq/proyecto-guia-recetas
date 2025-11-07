bd8a8744038f2ff9c78fa82eca20f618bb886229
Alexander <villafuertequispealex@gmail.com>
2025-11-07 18:55:24 -0500

    Correcion de importaciones en CrearRecetasScreen

--- app/src/main/java/com/example/proyectofinal/ui/screens/crud/CrearRecetasScreen.kt
+++ app/src/main/java/com/example/proyectofinal/ui/screens/crud/CrearRecetasScreen.kt
@@ -1,6 +1,5 @@
 package com.example.proyectofinal.ui.screens.crud
 
-import androidx.activity.result.launch
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -10,7 +9,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
