3a8f6928c7cfc29e3e7913423517e1d90a13e9a9
Alexander <villafuertequispealex@gmail.com>
2025-11-07 19:40:18 -0500

    Lista de recetas en pantalla

--- app/src/main/java/com/example/proyectofinal/ui/screens/crud/ListaRecetasScreen.kt
+++ app/src/main/java/com/example/proyectofinal/ui/screens/crud/ListaRecetasScreen.kt
@@ -2,22 +2,47 @@ package com.example.proyectofinal.ui.screens.crud
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.proyectofinal.data.Receta
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ListaRecetasScreen (onNavigateToBack: () -> Unit) {
+fun ListaRecetasScreen (
+    onNavigateToBack: () -> Unit,
+    viewModel: ListaRecetasViewModel = viewModel(factory = ListaRecetasViewModel.Factory)
+    ) {
+
+    // 'collectAsState' se asegura de que la UI se recomponga cuando el estado cambie.
+    val uiState by viewModel.uiState.collectAsState()
+
     Scaffold(
         topBar = {
             TopAppBar (
@@ -25,17 +50,77 @@ fun ListaRecetasScreen (onNavigateToBack: () -> Unit) {
                 colors = TopAppBarDefaults.topAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primary,
                     titleContentColor = MaterialTheme.colorScheme.onPrimary,
-                )
+                ),
+                navigationIcon = {
+                    IconButton(onClick = onNavigateToBack) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Volver",
+                            tint = MaterialTheme.colorScheme.onPrimary
+                        )
+                    }
+                }
             )
         }
     ) { innerPadding ->
-        Column(
-            modifier = Modifier.fillMaxSize().padding(innerPadding).padding(horizontal = 16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
+        // si esta vacio
+        if (uiState.listaRecetas.isEmpty()) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("No hay recetas guardadas")
+            }
+        } else {
             /*
             Lista de recetas
             */
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+                contentPadding = PaddingValues(16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                items(uiState.listaRecetas) { receta ->
+                    RecetaItem(receta = receta)
+                }
+            }
+
+        }
+    }
+}
+
+@Composable
+fun RecetaItem(receta: Receta, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Row {
+                Text(
+                    text = "Nombre:",
+                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.bodyLarge
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(text = receta.nombre, style = MaterialTheme.typography.bodyLarge)
+            }
+            Row {
+                Text(
+                    text = "Tipo:",
+                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(text = receta.tipo, style = MaterialTheme.typography.bodyMedium)
+            }
         }
     }
 }
